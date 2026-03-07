b3822e02f3fadaae274be1dd440d2093b5590f5f
liz0921 <lmonteror@ucenfotec.ac.cr>
2026-03-07 15:40:16 -0600

    refactor(FamilyLanding): Use generic FamilyMobile and FamilyLaptop with variant prop

--- src/components/FamilyLanding.js
+++ src/components/FamilyLanding.js
@@ -1,30 +1,30 @@
-import { FamilyMobileS } from "./FamilyMobileS"; //ok
-import { FamilyMobileM } from "./FamilyMobileM"; //ok
-import { FamilyMobileL } from "./FamilyMobileL"; // ok
-import FamilyTablet from "./FamilyTablet"; // ok
-import FamilyLaptop from "./FamilyLaptop"; //
-import FamilyLaptopL from "./FamilyLaptopL"; //
+import FamilyMobile from "./FamilyMobile";
+import FamilyLaptop from "./FamilyLaptop";
 
 const layouts = [
   {
     wrapperClass: "block min-[375px]:hidden",
     containerClass: "w-full min-h-screen relative mx-auto pb-[25px] overflow-x-auto",
-    Component: FamilyMobileS,
+    Component: FamilyMobile,
+    variant: "s",
   },
   {
     wrapperClass: "hidden min-[375px]:block min-[425px]:hidden",
     containerClass: "w-[375px] relative mx-auto pb-[60px]",
-    Component: FamilyMobileM,
+    Component: FamilyMobile,
+    variant: "m",
   },
   {
     wrapperClass: "hidden min-[425px]:block min-[768px]:hidden",
     containerClass: "w-[425px] min-h-screen relative mx-auto pb-[45px]",
-    Component: FamilyMobileL,
+    Component: FamilyMobile,
+    variant: "l",
   },
   {
     wrapperClass: "hidden min-[768px]:block min-[1024px]:hidden",
     containerClass: "w-[768px] min-h-screen relative mx-auto",
-    Component: FamilyTablet,
+    Component: FamilyMobile,
+    variant: "tablet",
   },
   {
     wrapperClass: "hidden min-[1024px]:block min-[1440px]:hidden overflow-auto",
@@ -34,17 +34,18 @@ const layouts = [
   {
     wrapperClass: "hidden min-[1440px]:block overflow-auto",
     containerClass: "w-full min-h-screen relative mx-auto",
-    Component: FamilyLaptopL,
+    Component: FamilyLaptop,
+    variant: "laptopL",
   },
 ];
 
 export const FamilyLanding = ({ data }) => {
   return (
     <div className="">
-      {layouts.map(({ wrapperClass, containerClass, Component }) => (
+      {layouts.map(({ wrapperClass, containerClass, Component, variant }) => (
         <div key={wrapperClass} className={wrapperClass}>
           <div className={containerClass}>
-            <Component data={data} />
+            <Component data={data} {...(variant ? { variant } : {})} />
           </div>
         </div>
       ))}
