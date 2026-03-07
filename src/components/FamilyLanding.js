542481512f94dc3e96f8c6812fe5e4bc2283165d
liz0921 <lmonteror@ucenfotec.ac.cr>
2026-03-07 12:21:48 -0600

    refactor: Streamline FamilyLanding component layout selection

--- src/components/FamilyLanding.js
+++ src/components/FamilyLanding.js
@@ -4,45 +4,50 @@ import { FamilyMobileL } from "./FamilyMobileL"; // ok
 import FamilyTablet from "./FamilyTablet"; // ok
 import FamilyLaptop from "./FamilyLaptop"; //
 import FamilyLaptopL from "./FamilyLaptopL"; //
-import FamilyDesktop from "./FamilyDesktop"; // 
-import FamilyDesktopL from "./FamilyDesktopL"; // 
+
+const layouts = [
+  {
+    wrapperClass: "block min-[375px]:hidden",
+    containerClass: "w-full min-h-screen relative mx-auto pb-[25px] overflow-x-auto",
+    Component: FamilyMobileS,
+  },
+  {
+    wrapperClass: "hidden min-[375px]:block min-[425px]:hidden",
+    containerClass: "w-[375px] relative mx-auto pb-[60px]",
+    Component: FamilyMobileM,
+  },
+  {
+    wrapperClass: "hidden min-[425px]:block min-[768px]:hidden",
+    containerClass: "w-[425px] min-h-screen relative mx-auto pb-[45px]",
+    Component: FamilyMobileL,
+  },
+  {
+    wrapperClass: "hidden min-[768px]:block min-[1024px]:hidden",
+    containerClass: "w-[768px] min-h-screen relative mx-auto",
+    Component: FamilyTablet,
+  },
+  {
+    wrapperClass: "hidden min-[1024px]:block min-[1440px]:hidden overflow-auto",
+    containerClass: "w-full min-h-screen relative mx-auto",
+    Component: FamilyLaptop,
+  },
+  {
+    wrapperClass: "hidden min-[1440px]:block overflow-auto",
+    containerClass: "w-full min-h-screen relative mx-auto",
+    Component: FamilyLaptopL,
+  },
+];
+
 export const FamilyLanding = ({ data }) => {
   return (
     <div className="">
-
-      {/* Mobile S - 320px ok*/}
-      <div className="block min-[375px]:hidden">
-        <div className="w-full min-h-screen relative mx-auto pb-[25px] overflow-x-auto">
-          <FamilyMobileS data={data} />
+      {layouts.map(({ wrapperClass, containerClass, Component }) => (
+        <div key={wrapperClass} className={wrapperClass}>
+          <div className={containerClass}>
+            <Component data={data} />
+          </div>
         </div>
-      </div>
-
-      {/* Mobile M - 375px ok*/}
-      <div className="hidden min-[375px]:block min-[425px]:hidden">
-        <div className="w-[375px] relative mx-auto pb-[60px]">
-          <FamilyMobileM data={data} />
-        </div>
-      </div>
-
-      {/* Mobile L - 425px ok*/}
-      <div className="hidden min-[425px]:block min-[768px]:hidden">
-        <div className="w-[425px] min-h-screen relative mx-auto pb-[45px]">
-          <FamilyMobileL data={data} />
-        </div>
-      </div>
-      {/* FamiliTablet - 768px ok*/}
-      <div className="hidden min-[768px]:block min-[1024px]:hidden">
-        <div className="w-[768px] min-h-screen relative mx-auto">
-          <FamilyTablet data={data} />
-        </div>
-      </div>
-      {/* Laptop - 1024px */}
-      <div className="hidden min-[1024px]:block overflow-auto">
-        <div className="w-full min-h-screen relative mx-auto ">
-          <FamilyLaptop data={data} />
-        </div>
-      </div>
- 
+      ))}
     </div>
   );
 };
