87bdd95653dec61f80c89ce86d3bd2b8078fed65
liz0921 <lmonteror@ucenfotec.ac.cr>
2026-03-07 15:40:17 -0600

    feat(FamilyMobile): Implement responsive styles for mobile and tablet variants

--- src/components/FamilyMobile.js
+++ src/components/FamilyMobile.js
@@ -35,8 +35,11 @@ const stylesByVariant = {
     ctaLink: "absolute items-center justify-center text-beige pl-2",
     ctaSvg: "w-[10em] h-[35px] z-10",
     ctaText: "relative font-txt justify-center text-center text-[16px] -top-[29px] z-20 uppercase",
+    headingSize: "text-xl",
+    locationSize: "text-md font-txt",
     descriptionClass: "text-[12px] pb-5 pt-[5vh] text-darkBlue font-txt",
     descriptionMode: "split4",
+    dividerClass: "",
     mediaWrap: "relative w-full h-[400px] -top-5 -left-2",
     mediaTopWrap: "relative h-[200px]",
     videoWrap: "absolute top-1 right-[3.5em] w-[250px] h-[110px]",
@@ -44,6 +47,7 @@ const stylesByVariant = {
     videoFrameHeight: 125,
     videoFrameClass: "transform-gpu w-[228px] h-[125px]",
     videoFrameSizes: "(max-width: 375px) 228px",
+    videoInnerClass: "absolute inset-0 flex items-center justify-center",
     videoClass:
       "w-[80%] h-[90%] mt-3 -ml-5 object-cover border-darkBlue border-[6px] rounded-sm z-20 shadow-gray-500 shadow-[2px_-2px_5px_rgba(0,0,0,0.3)] hover:opacity-100 [&::-webkit-media-controls]:opacity-0 hover:[&::-webkit-media-controls]:opacity-100 [&::-webkit-media-controls]:transition-opacity",
     photo2Wrap: "absolute left-[11.5em] top-0 w-[10em] h-[13em] z-10",
@@ -83,8 +87,11 @@ const stylesByVariant = {
     ctaLink: "absolute items-center justify-center text-beige pl-5",
     ctaSvg: "w-[10em] h-[35px] z-10",
     ctaText: "relative font-txt justify-center text-center text-[16px] -top-[29px] z-20 uppercase",
+    headingSize: "text-xl",
+    locationSize: "text-md font-txt",
     descriptionClass: "text-[10px] pb-6 pt-11 text-darkBlue font-txt text-left",
     descriptionMode: "split2",
+    dividerClass: "",
     mediaWrap: "relative w-full h-[400px] -top-5 -left-2",
     mediaTopWrap: "relative h-[200px]",
     videoWrap: "absolute left-3 top-1.5 w-[200.79px] h-[118.43px]",
@@ -92,6 +99,7 @@ const stylesByVariant = {
     videoFrameHeight: 118.43,
     videoFrameClass: "transform-gpu relative -left-[14px] h-[135px]",
     videoFrameSizes: undefined,
+    videoInnerClass: "absolute inset-0 flex items-center justify-center",
     videoClass:
       "w-[187.97px] h-[106.42px] mt-3 -ml-5 object-cover border-darkBlue border-[6px] rounded-sm z-20 shadow-gray-500 shadow-[2px_-2px_5px_rgba(0,0,0,0.3)] hover:opacity-100 [&::-webkit-media-controls]:opacity-0 hover:[&::-webkit-media-controls]:opacity-100 [&::-webkit-media-controls]:transition-opacity",
     photo2Wrap: "absolute left-[12em] top-0 w-[10em] h-[10em] z-10",
@@ -131,8 +139,11 @@ const stylesByVariant = {
     ctaLink: "absolute inset-0 items-center justify-center text-beige w-[70%] left-4",
     ctaSvg: "w-[80%] h-[35px] z-10",
     ctaText: "relative font-txt uppercase justify-center text-[20px] -top-[32px] z-20 w-[80%] text-center",
+    headingSize: "text-2xl",
+    locationSize: "text-xl font-txt upercase",
     descriptionClass: "text-[14px] pb-6 pt-5 text-darkBlue font-txt",
     descriptionMode: "split4",
+    dividerClass: "border-more-dashed pb-2",
     mediaWrap: "w-full h-[auto] relative -top-5 -left-2 mb-[70px]",
     mediaTopWrap: "relative h-[200px]",
     videoWrap: "absolute -left-0 -top-2.5 w-[298px] h-[140px]",
@@ -140,6 +151,7 @@ const stylesByVariant = {
     videoFrameHeight: 100,
     videoFrameClass: "transform-gpu",
     videoFrameSizes: undefined,
+    videoInnerClass: "absolute inset-0 flex items-center justify-center",
     videoClass:
       "w-[82%] h-[90%] mt-3 -ml-6 object-cover border-darkBlue border-[6px] rounded-sm z-20 shadow-gray-500 shadow-[2px_-2px_5px_rgba(0,0,0,0.3)] hover:opacity-100 [&::-webkit-media-controls]:opacity-0 hover:[&::-webkit-media-controls]:opacity-100 [&::-webkit-media-controls]:transition-opacity",
     photo2Wrap: "absolute -right-[2px] -top-4 w-[175px] z-10",
@@ -162,6 +174,58 @@ const stylesByVariant = {
     showDrawing: true,
     drawingClass: "w-[170px] h-[125px] absolute -bottom-[110px] -right-[4px] z-20 brightness-[78%] contrast-[91%] opacity-[70%]",
   },
+  tablet: {
+    outer: "relative top-[12vh] p-10",
+    panel: "border-[3px] border-darkBeige p-16 relative",
+    headingWrap: "text-left border-b-[3px] border-darkBeige border-dashed",
+    venueWrap: "absolute w-[50%] top-[18vh] right-[-3vw]",
+    venueBox: "bg-darkBlue p-6 rounded-3xl",
+    venueTitle: "text-lightRed text-[25px] font-titles leading-none",
+    venueText: "text-beige font-txt leading-tight text-[15px]",
+    venueLink: "text-beige mt-1 font-titles underline text-[16px]",
+    infoWrap: "",
+    infoRow: "flex items-center gap-2 text-darkBlue font-txt",
+    iconClass: "text-[13px]",
+    infoText: "text-[12px] font-bold",
+    ctaWrap: "relative h-[8vh] mt-2 -left-[2.5vw]",
+    ctaLink: "absolute inset-0 items-center justify-center text-beige w-[65%] left-4",
+    ctaSvg: "w-[80%] h-[45px] z-10",
+    ctaText: "relative font-txt uppercase justify-center text-[26px] -top-[42px] z-20 w-[80%] text-center",
+    headingSize: "text-[40px]",
+    locationSize: "text-[25px] font-txt lowercase",
+    descriptionClass: "text-[14px] pb-6 pt-5 text-darkBlue font-txt",
+    descriptionMode: "split4",
+    dividerClass: "border-more-dashed my-2",
+    mediaWrap: "w-full h-[auto] relative",
+    mediaTopWrap: "relative h-[220px]",
+    videoWrap: "absolute -top-[6em] w-[660px] h-[380px]",
+    videoFrameWidth: 460,
+    videoFrameHeight: 300,
+    videoFrameClass: "h-[300px]",
+    videoFrameSizes: undefined,
+    videoInnerClass: "absolute inset-0 flex items-center justify-center right-[10em] bottom-[5.3em]",
+    videoClass:
+      "w-[28em] h-[15em] object-cover border-darkBlue border-[10px] rounder-sm z-20 shadow-gray-500 shadow-[2px_-2px_5px_rgba(0,0,0,0.3)] hover:opacity-100 [&::-webkit-media-controls]:opacity-0 hover:[&::-webkit-media-controls]:opacity-100 [&::-webkit-media-controls]:transition-opacity",
+    photo2Wrap: "absolute left-[26.5em] bottom-[2em] w-[18em] z-10",
+    photo2Class: "w-full shadow-gray-500 shadow-[3px_3px_3px_rgba(0,0,0,0.3)]",
+    mediaBottomWrap: "relative h-[750px] mx-5 mb-4",
+    photo1Wrap: "absolute left-[2em] -top-[4em] z-10 w-1/2",
+    photo1Class: "z-10 h-[320px] shadow-gray-500 shadow-[0px_3px_3px_rgba(0,0,0,0.3)]",
+    backWrap: "relative",
+    backImage: "absolute -left-[18px] top-[20px] w-[290px] h-[110px] cursor-pointer",
+    backText: "absolute inset-0 font-titles text-beige w-3/4 text-center mx-auto top-[40px] text-[50px]",
+    photo3Wrap: "absolute left-[21.5em] bottom-[30em] w-[350px] h-[300px]",
+    photo3Class: "w-full h-auto",
+    titleWrap: "absolute bottom-[10em] left-[15px] z-10",
+    titleLeading: "flex flex-col leading-[75px] mb-10",
+    titleSize: "text-darkBlue font-titles text-[90px]",
+    subtitleWrap: "text-lightRed text-[15px] flex flex-col",
+    showAnchor: true,
+    anchorClass: "ml-[25px] rotate-12 mb-4",
+    subtitleTextWrap: "-ml-[50px] mb-[30px] leading-none tracking-widest font-txt uppercase text-[20px]",
+    showDrawing: true,
+    drawingClass: "absolute bottom-[12em] right-[0px] z-20 brightness-[78%] contrast-[91%] opacity-[70%] w-[300px] h-[300px]",
+  },
 };
 
 export const FamilyMobile = ({ data, variant = "s" }) => {
@@ -176,15 +240,15 @@ export const FamilyMobile = ({ data, variant = "s" }) => {
       <div className={styles.panel}>
         <div className={styles.headingWrap}>
           <h1 className="font-titles leading-none">
-            <span className={variant === "l" ? "text-darkBlue text-2xl" : "text-darkBlue text-xl"}>old time sailors </span>
-            <span className={variant === "l" ? "text-lightRed text-2xl" : "text-lightRed text-xl"}> at</span>
+            <span className={`text-darkBlue ${styles.headingSize}`}>old time sailors </span>
+            <span className={`text-lightRed ${styles.headingSize}`}> at</span>
             <br />
-            <span className={variant === "l" ? "text-lightRed text-2xl" : "text-lightRed text-xl"}>{event}</span>
+            <span className={`text-lightRed ${styles.headingSize}`}>{event}</span>
           </h1>
-          <p className={variant === "l" ? "text-darkBlue text-xl font-txt upercase" : "text-darkBlue text-md font-txt"}>{location}</p>
+          <p className={`text-darkBlue ${styles.locationSize}`}>{location}</p>
         </div>
 
-        {variant === "l" && <div className="border-more-dashed pb-2" />}
+        {styles.dividerClass && <div className={styles.dividerClass} />}
 
         <div className={styles.venueWrap}>
           <div className={styles.venueBox}>
@@ -271,7 +335,7 @@ export const FamilyMobile = ({ data, variant = "s" }) => {
               priority={true}
               sizes={styles.videoFrameSizes}
             />
-            <div className="absolute inset-0 flex items-center justify-center">
+            <div className={styles.videoInnerClass}>
               <video controls preload="none" poster={"/assets/thumbnailvideo.webp"} className={styles.videoClass}>
                 <source src="/assets/familyVideo.mp4" type="video/mp4" />
               </video>
