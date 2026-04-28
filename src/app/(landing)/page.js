f7b38ea52a7547d99222c0d2c7fe08d04375f786
liz0921 <85530633+liz0921@users.noreply.github.com>
2026-04-28 13:12:41 -0600

    btn

--- src/app/(landing)/page.js
+++ src/app/(landing)/page.js
@@ -451,7 +451,7 @@ export default async function Home() {
               className="
                 absolute
                 right-[1.8rem]
-                top-[5.2rem]
+                top-[5rem]
                 flex flex-col items-end
                 gap-[0.08rem]
                 scale-[0.82]
