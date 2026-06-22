f43382c5ca9c1d661d0c94558597681d9d4d5a81
liz0921 <85530633+liz0921@users.noreply.github.com>
2026-06-22 14:12:39 -0600

    btn social media

--- src/app/(landing)/page.js
+++ src/app/(landing)/page.js
@@ -325,7 +325,7 @@ export default async function Home() {
               </nav>
             </div>
 
-            <div className="absolute left-1/2 bottom-[21px] z-20 -translate-x-1/2">
+            <div className="absolute left-1/2 bottom-[10px] z-20 -translate-x-1/2">
               <div className="flex w-fit items-center justify-center gap-[35px]">
                 {socialItems.map((item) => (
                   <SocialIcon key={item.alt} href={item.href} src={item.src} alt={item.alt} />
